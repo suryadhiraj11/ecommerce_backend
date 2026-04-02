ee85902d5be7598197f31e22dc466260b96fbc21
ACHANTA SURYA DHIRAJ <2400032511@kluniversity.in>
2026-04-02 09:10:13 +0530

    Update SecurityConfig.java

--- src/main/java/com/klu/config/SecurityConfig.java
+++ src/main/java/com/klu/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "https://ecommerce-frontend-production-2779.up.railway.app"
+            "https://ecommercefrontend-production-8119.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
